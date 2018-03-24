615e86174d57e74719ee28232099a97243a169ba
Amrita <y.amrita@gmail.com>
2018-03-25 01:28:32 +0530

    fixed routing, removed search.

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'manageDevices', loadChildren: './charts/charts.module#ChartsModule' },
+            { path: 'manageDevices', loadChildren: './manage-devices/manage-devices.module#ManageDevicesModule' },
             { path: 'notifications', loadChildren: './tables/tables.module#TablesModule' },
           /*  { path: 'forms', loadChildren: './form/form.module#FormModule' },
             { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
